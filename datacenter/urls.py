785357a7ce8af52ded92b0bc5b11bc6571045464
Riccardo GIannetto <rgiannetto@gmail.com>
2025-02-02 21:12:12 +0100

    openapi schema

--- datacenter/urls.py
+++ datacenter/urls.py
@@ -14,9 +14,45 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .permissions import AccessListPermission
+from django.conf.urls.static import static
+from django.conf import settings
+# from Blog import urls as blog_urls
+from rest_framework import permissions
+from rest_framework.schemas import get_schema_view
+from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+schema_url_patterns = [
+    # path('blog/', include(blog_urls.urlpatterns)),
+]
+
+urlpatterns = [
+    # path("blog/", include(blog_urls.urlpatterns)),
+    path('admin/', admin.site.urls),
+    path('', get_schema_view(
+         title="Datacenter API",
+         description="API app Datacenter",
+         version="1.0.0",
+         patterns=schema_url_patterns,
+         public=True,
+         permission_classes=[AccessListPermission |
+                             permissions.IsAuthenticated]
+         ), name='openapi-schema'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
