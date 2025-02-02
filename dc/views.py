1090403da90a8bceb7f0e8be725ff1a50a326887
Riccardo GIannetto <rgiannetto@gmail.com>
2025-02-02 22:55:31 +0100

    Add cors support

--- dc/views.py
+++ dc/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 from .models import DataCenterLocation
 from .serializers import DataCenterLocationSerializer
 
@@ -6,3 +7,5 @@ from .serializers import DataCenterLocationSerializer
 class DataCenterLocationViewSet(viewsets.ModelViewSet):
     queryset = DataCenterLocation.objects.all()
     serializer_class = DataCenterLocationSerializer
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
