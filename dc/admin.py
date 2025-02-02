04c12c1040a1379d897ddb015108014ff5d4667f
Riccardo GIannetto <rgiannetto@gmail.com>
2025-02-02 21:38:18 +0100

    Datacenter location ws initial support

--- dc/admin.py
+++ dc/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import DataCenterLocation
 
-# Register your models here.
+
+@admin.register(DataCenterLocation)
+class DataCenterLocationAdmin(admin.ModelAdmin):
+    save_on_top = True
+    fields = [
+        ('name', 'location'),
+        ('capacity', 'operational_since'),
+    ]
+
+    list_display = ('name', 'location', 'capacity',
+                    'operational_since')
+    search_fields = ['name', 'location']
+    readonly_fields = ['operational_since']
--- dc/models.py
+++ dc/models.py
@@ -5,7 +5,7 @@ class DataCenterLocation(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     capacity = models.IntegerField()
-    operational_since = models.DateField()
+    operational_since = models.DateTimeField(auto_now_add=True, editable=True)
 
     def __str__(self):
         return self.name
