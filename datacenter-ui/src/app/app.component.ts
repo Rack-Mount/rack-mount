44de6d20d38afd70c802b4dfdcedaa3d0e7229bb
Riccardo GIannetto <rgiannetto@gmail.com>
2025-02-02 22:55:45 +0100

    Test datacenter Service

--- datacenter-ui/src/app/app.component.ts
+++ datacenter-ui/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatacenterService } from './modules/core/api/v1';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'datacenter-ui';
+  constructor(private readonly datacenterService: DatacenterService) {}
+
+  ngOnInit() {
+    this.datacenterService.listDataCenterLocations().subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
--- datacenter-ui/src/app/app.config.ts
+++ datacenter-ui/src/app/app.config.ts
@@ -1,8 +1,37 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  ApiModule,
+  Configuration,
+  ConfigurationParameters,
+} from './modules/core/api/v1';
+import { environment } from '../environments/environment';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+export function apiConfigFactory(): Configuration {
+  const params: ConfigurationParameters = {
+    basePath: environment.service_url,
+  };
+  return new Configuration(params);
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'disabled',
+        scrollPositionRestoration: 'top',
+      })
+    ),
+    importProvidersFrom(ApiModule.forRoot(apiConfigFactory)),
+    provideHttpClient(withFetch()),
+  ],
 };
