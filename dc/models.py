f99957e78bed396c2b2e90ec6dad742c72f46b8d
Riccardo GIannetto <rgiannetto@gmail.com>
2025-02-03 05:22:17 +0100

    DataCenterLocation Model

--- dc/models.py
+++ dc/models.py
@@ -7,7 +7,7 @@ class DataCenterLocation(models.Model):
         max_length=15, blank=False, null=False, unique=True
     )
     location = models.CharField(max_length=100)
-    capacity = models.IntegerField()
+    capacity = models.PositiveIntegerField(default=0, null=False)
     operational_since = models.DateTimeField(auto_now_add=True, editable=True)
     manager = models.CharField(max_length=100, blank=True)
     manager_mail = models.EmailField(blank=True, null=True)
