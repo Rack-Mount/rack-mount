170f43c38abfc08783b0e16aea58ec9aa900d97b
Riccardo GIannetto <rgiannetto@gmail.com>
2025-02-05 22:23:22 +0100

    Uptade models

--- dc/models/LocationCustomField.py
+++ dc/models/LocationCustomField.py
@@ -3,8 +3,8 @@ from dc.models import Location
 
 
 class LocationCustomField(models.Model):
-    data_center_location = models.ForeignKey(
-        Location, on_delete=models.CASCADE)
+    location = models.ForeignKey(
+        Location, related_name='custom_fields', on_delete=models.CASCADE)
     field_name = models.CharField(max_length=100, blank=False, null=False)
     field_value = models.CharField(max_length=255, blank=True)
 
--- dc/serializers/LocationSerializer.py
+++ dc/serializers/LocationSerializer.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from dc.models import Location
+from dc.serializers import LocationCustomFieldSerializer
 
 
 class LocationSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
         read_only=True, view_name='location-detail')
+    custom_fields = LocationCustomFieldSerializer(
+        many=True, read_only=True)
 
     class Meta:
         model = Location
