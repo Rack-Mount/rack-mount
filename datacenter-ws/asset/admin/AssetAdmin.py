eb539b8e7cd4957178e84ba94a48f353463695da
Riccardo GIannetto <rgiannetto@gmail.com>
2025-02-09 17:31:46 +0100

    add asset export

--- datacenter-ws/asset/admin/AssetAdmin.py
+++ datacenter-ws/asset/admin/AssetAdmin.py
@@ -3,13 +3,25 @@ from asset.models import Asset, AssetState, Rack
 from asset.admin import AssetCustomFieldInline
 from datacenter.models import Location
 from reversion.admin import VersionAdmin
-from django import forms
+from import_export.admin import ExportActionModelAdmin
+from import_export import resources
+
+
+class AssetResource(resources.ModelResource):
+
+    class Meta:
+        model = Asset
+        fields = ('hostname', 'rack__location__name', 'rack__name', 'model__type__name', 'model__name', 'model__vendor__name',
+                  'serial_number', 'sap_id', 'order_id', 'model__rack_units', 'power_cosumption_watt')
+        name = "Export/Import assets"
 
 
 @admin.register(Asset)
-class AssetAdmin(VersionAdmin):
+class AssetAdmin(ExportActionModelAdmin, VersionAdmin):
     save_on_top = True
     save_as = True
+
+    resource_classes = [AssetResource]
 
     list_display = ('hostname', 'rack__location', 'rack__name', 'model__type', 'model__name', 'model__vendor',
                     'serial_number', 'sap_id', 'order_id', 'model__rack_units', 'power_cosumption_watt', 'state')
