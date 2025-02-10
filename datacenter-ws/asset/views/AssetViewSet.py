cbef0e17d25e879c8876f840b1e1be46daeeba47
Riccardo GIannetto <rgiannetto@gmail.com>
2025-02-10 17:16:57 +0100

    Pagination refactoring

--- datacenter-ws/asset/views/AssetViewSet.py
+++ datacenter-ws/asset/views/AssetViewSet.py
@@ -1,16 +1,9 @@
 from rest_framework import viewsets
 from asset.serializers import AssetSerializer
 from asset.models import Asset
-from rest_framework.pagination import PageNumberPagination
 from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
 import django_filters.rest_framework
-
-
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 5
-    page_size_query_param = 'page_size'
-    max_page_size = 100
+from asset.paginations import StandardResultsSetPagination
 
 
 class AssetViewSet(viewsets.ModelViewSet):
