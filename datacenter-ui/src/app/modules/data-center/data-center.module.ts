4213b789eed2faf37bd5778cbb7b845ce0b92094
Riccardo GIannetto <rgiannetto@gmail.com>
2025-02-12 18:46:54 +0100

    add dataceneter module

---
+++ datacenter-ui/src/app/modules/data-center/data-center.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DataCenterRoutingModule } from './data-center-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DataCenterRoutingModule
+  ]
+})
+export class DataCenterModule { }
