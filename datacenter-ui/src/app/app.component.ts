16897ef523aaa93081e6757dfc817464599cf140
Riccardo GIannetto <rgiannetto@gmail.com>
2025-02-12 18:48:03 +0100

    add rack test import

--- datacenter-ui/src/app/app.component.ts
+++ datacenter-ui/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatacenterService } from './modules/core/api/v1';
+import { RackComponent } from './modules/data-center/components/rack/rack.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RackComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
