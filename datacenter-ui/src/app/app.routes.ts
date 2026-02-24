d2c3c41cd3cc74ce1de0ee3c5ebe80b21b5330be
Riccardo GIannetto <rgiannetto@gmail.com>
2026-02-24 20:02:49 +0100

    feat: implement routing for RackComponent and enhance its functionality with asset retrieval

--- datacenter-ui/src/app/app.routes.ts
+++ datacenter-ui/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { RackComponent } from './modules/data-center/components/rack/rack.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'rack/:id', component: RackComponent }];
--- datacenter-ui/src/app/modules/data-center/components/rack/rack.component.ts
+++ datacenter-ui/src/app/modules/data-center/components/rack/rack.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import {
   AssetRackUnitListRequestParams,
   AssetService,
@@ -15,46 +22,73 @@ import { DeviceComponent } from '../device/device.component';
   templateUrl: './rack.component.html',
   styleUrl: './rack.component.scss',
 })
-export class RackComponent implements OnChanges {
+export class RackComponent implements OnChanges, OnInit {
   @Input() rack: Rack | undefined;
   assets: RackUnit[] = [];
   rackRender: RackRender[] = [];
 
-  constructor(private readonly assetService: AssetService) {}
-  ngOnChanges(changes: SimpleChanges): void {
-    if (this.rack) {
-      for (let i = 0; i < this.rack.model.capacity; i++) {
-        this.rackRender.push({
-          rack_unit: 1,
-          position: this.rack.model.capacity - i,
-          visible: true,
+  constructor(
+    private readonly assetService: AssetService,
+    private readonly route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      if (id) {
+        this.assetService.assetRackRetrieve({ name: id }).subscribe((rack) => {
+          this.rack = rack;
+          this.renderRack();
         });
       }
+    });
 
-      const params: AssetRackUnitListRequestParams = {
-        rackName: this.rack.name,
-        pageSize: this.rack.model.capacity,
-      };
-
-      this.assetService.assetRackUnitList(params).subscribe((assets) => {
-        this.assets = assets.results;
-        this.assets.forEach((asset) => {
-          console.log(asset.position);
-          this.rackRender[this.rack!.model.capacity - asset.position - 1] = {
-            device: asset,
-            rack_unit: asset.device_rack_units,
-            visible: true,
-          };
-          if (asset.device_rack_units > 1) {
-            for (let i = 1; i < asset.device_rack_units; i++) {
-              this.rackRender[
-                this.rack!.model.capacity - asset.position - i + 1
-              ].visible = false;
-            }
-          }
-        });
-      });
-      console.log(this.rackRender);
+    if (this.rack) {
+      this.renderRack();
     }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['rack'] && this.rack) {
+      this.renderRack();
+    }
+  }
+
+  private renderRack(): void {
+    if (!this.rack) return;
+    this.rackRender = []; // Clear previous render
+
+    for (let i = 0; i < this.rack.model.capacity; i++) {
+      this.rackRender.push({
+        rack_unit: 1,
+        position: this.rack.model.capacity - i,
+        visible: true,
+      });
+    }
+
+    const params: AssetRackUnitListRequestParams = {
+      rackName: this.rack.name,
+      pageSize: this.rack.model.capacity,
+    };
+
+    this.assetService.assetRackUnitList(params).subscribe((assets) => {
+      this.assets = assets.results;
+      this.assets.forEach((asset) => {
+        console.log(asset.position);
+        this.rackRender[this.rack!.model.capacity - asset.position - 1] = {
+          device: asset,
+          rack_unit: asset.device_rack_units,
+          visible: true,
+        };
+        if (asset.device_rack_units > 1) {
+          for (let i = 1; i < asset.device_rack_units; i++) {
+            this.rackRender[
+              this.rack!.model.capacity - asset.position - i + 1
+            ].visible = false;
+          }
+        }
+      });
+    });
+    console.log(this.rackRender);
   }
 }
