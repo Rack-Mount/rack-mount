dc60309e683a25f4da0db1f5db8ed4753c40baa4
Riccardo GIannetto <rgiannetto@gmail.com>
2026-02-24 20:02:44 +0100

    refactor: clean up app component by removing unused imports and commented code

--- datacenter-ui/src/app/app.component.ts
+++ datacenter-ui/src/app/app.component.ts
@@ -10,30 +10,14 @@ import { RackComponent } from './modules/data-center/components/rack/rack.compon
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RackComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'datacenter-ui';
-  rack: Rack | undefined;
 
-  constructor(
-    private readonly datacenterService: DatacenterService,
-    private readonly assetService: AssetService
-  ) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.datacenterService.datacenterLocationList().subscribe((data) => {
-      console.log(data);
-    });
-
-    const rack_params: AssetRackRetrieveRequestParams = {
-      name: '19',
-    };
-
-    this.assetService.assetRackRetrieve(rack_params).subscribe((rack) => {
-      this.rack = rack;
-    });
-  }
+  ngOnInit() {}
 }
